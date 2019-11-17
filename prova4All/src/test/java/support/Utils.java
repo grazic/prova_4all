1785f5bb845f295c57192886081f5dc3aa62aecf
unknown <grazi.cerentini@gmail.com>
2019-11-16 23:14:06 -0300

    pasta para screenshot atualizada

--- prova4All/src/test/java/support/Utils.java
+++ prova4All/src/test/java/support/Utils.java
@@ -14,12 +14,13 @@ public class Utils {
 	static int screenshotCount = 0;
 	
 	//Método para tirar screenshot
+	//Para visualizar as imagens, atualizar o projeto após execução. Na pasta src/screenshot será exibida as imagens
 	public static void takeScreenshot(WebDriver driver) {	
 		
 		TakesScreenshot driverScr = (TakesScreenshot)driver;
 		byte[] scr = driverScr.getScreenshotAs(OutputType.BYTES);
 		try {
-			Files.write(scr, new File("screenshot/scr" + screenshotCount + ".png"));
+			Files.write(scr, new File("src/screenshot" + screenshotCount + ".png"));
 			screenshotCount++;
 			// FileUtils.copyFile(scrFile, new File(
 			// "C:\\Users\\graziela.cerentini\\eclipse-workspace\\studyingAutomation\\studyingAutomation\\screenshot"));
